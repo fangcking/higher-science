f7497aaaabcaab8329ace706780f96762409c5a6
fangcking <1667513703@qq.com>
2018-10-17 18:15:08 +0800

    只修改get即可

--- Components/getdatabehavior.js
+++ Components/getdatabehavior.js
@@ -8,9 +8,9 @@ module.exports = Behavior({
     oriData: Array
   },
   data: {
-    localdata: {}
+   localdata :{}
   },
-  ready: function () {
+  ready: function(){
     this.update()
   },
   methods: {
@@ -18,52 +18,45 @@ module.exports = Behavior({
       // 本地读取
       let that = this
       let getdata = that.properties.oriData;
+      console.log(getdata)
       wx.getStorage({
         key: this.properties.getDataname,
-        // success: res => {
-        //   console.log('获取到本地有数据')
-        //   // 内置数据加用户设置数据
-        //   getdata.push(...res.data);
-        // },
-        fail: () => {
+        success: res =>{
+          console.log('获取到本地有数据')
+          // 内置数据加用户设置数据
+           getdata.push(...res.data);
+        },
+        fail:()=> {
           console.log('本地mei有数据')
           wx.showToast({
             title: 'sorry，暂未获取到您的信息',
-            image: '/images/err.png'
+            image :'/images/err.png'
           })
         },
-        complete: () => {
+        complete:()=> {
           //  去重
-          console.log(getdata)
-          // let a ={}
-          // for(let value of getdata){
-          //    a = {
-          //      name:value.name,
-          //      id :value.id
-          //    }
-          // }
-          // getdata = getdata.push(a)
+          getdata = [...new Set(getdata)]
           that.setData({
             localdata: {
               key: that.properties.getDataname,
               datavalue: getdata
             },
           })
-          that.triggerEvent('getdataback', getdata.name)
+          that.triggerEvent('getdataback', getdata)
           wx.request({
             url: 'http://localhost:8083/',
             data: that.properties.getDataname,
-            success: a => {
+            success:a=> {
               // 再次触发事件传回后台数据
               // 对比前后台数据，若不符摒弃客户端数据
               // 设置后台传回数据为data
-              let res = JSON.parse(a)
+              let res =JSON.parse(a)
               // datacode 为0表示
-              if (res.data === 0) {
+              if(res.data ===0){
                 that.setData({
                   localdata: {
                     key: that.properties.getDataname,
-                    data: getdata
+                    datavalue: getdata
                   },
                 })
               }
